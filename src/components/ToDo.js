881828c8b9ed9560032020312234dd1c81c6cf2a
Ali Ashraf <ali.ashraf4915@gmail.com>
2023-06-28 13:42:56 +0500

    updated with redux

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,116 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Button } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import DoneIcon from "@mui/icons-material/Done";
-import EditIcon from "@mui/icons-material/Edit";
-import { red } from "@mui/material/colors";
+import { useDispatch } from "react-redux";
+import { pagereload } from "../Actions/index";
+import List from "./List";
+import SaveForm from "./SaveForm";
+import EditForm from "./EditForm";
 
 export default function ToDo() {
-  const [text, setText] = useState("");
-  const [savedToDos, setsavedToDos] = useState([]);
-  const [editIndex, setEditIndex] = useState(-1);
-
+  const dispatch = useDispatch();
+  const [edit, setEdit] = useState(false);
+  const [obj, setobj] = useState({ id: "", todo: "", completed: "" });
+  const [todoValue, setTodoValue] = useState("");
   useEffect(() => {
-    const saveditems = localStorage.getItem("savedToDos");
-    if (saveditems) {
-      setsavedToDos(JSON.parse(saveditems));
-    }
+    dispatch(pagereload());
   }, []);
 
-  const handleChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const handleSave = (event) => {
-    event.preventDefault();
-    if (text !== null) {
-      if (editIndex >= 0) {
-        const updatedToDos = [...savedToDos];
-        if (updatedToDos[editIndex].done == false) {
-          updatedToDos[editIndex].text = text;
-          setsavedToDos(updatedToDos);
-          localStorage.setItem("savedToDos", JSON.stringify(updatedToDos));
-          setEditIndex(-1);
-          setText("");
-        } else if (updatedToDos[editIndex].done == true) {
-          setEditIndex(-1);
-          setText("");
-        }
-      } else {
-        const updatedTexts = [...savedToDos, { text, done: false }];
-        setsavedToDos(updatedTexts);
-        localStorage.setItem("savedToDos", JSON.stringify(updatedTexts));
-        setEditIndex(-1);
-        setText("");
-      }
-    }
-  };
-
-  const handleEdit = (index) => {
-    const todoToEdit = savedToDos[index];
-    setText(todoToEdit.text);
-    setEditIndex(index);
-  };
-  const handleDone = (index) => {
-    const updatedToDos = [...savedToDos];
-    updatedToDos[index].done = true;
-    setsavedToDos(updatedToDos);
-    localStorage.setItem("savedToDos", JSON.stringify(updatedToDos));
-    setEditIndex(-1);
-    setText("");
-  };
-  const handleDelete = (index) => {
-    const updatedToDos = savedToDos.filter((_, i) => i !== index);
-    setsavedToDos(updatedToDos);
-    localStorage.setItem("savedToDos", JSON.stringify(updatedToDos));
-  };
   return (
-    <div style={{ margin: "20px" }}>
-      <form onSubmit={handleSave}>
-        <TextField
-          id="outlined-basic"
-          label="ToDo"
-          variant="outlined"
-          value={text}
-          onChange={handleChange}
+    <>
+      {edit === false ? (
+        <SaveForm todoValue={todoValue} setTodoValue={setTodoValue} />
+      ) : (
+        <EditForm
+          todoValue={todoValue}
+          setobj={setobj}
+          obj={obj}
+          setTodoValue={setTodoValue}
+          setEdit={setEdit}
         />
-        <Button
-          sx={{ marginLeft: "4px", height: "55px" }}
-          variant="contained"
-          type="submit"
-          disabled={text == "" && true}
-        >
-          Save
-        </Button>
-      </form>
-
-      <div>
-        {savedToDos.map((savedItem, index) => {
-          const todoStyle = {
-            textDecoration: savedItem.done ? "line-through" : "none",
-          };
-          return (
-            <div key={index} style={todoStyle}>
-              {savedItem.text}
-              <Button
-                onClick={() => handleEdit(index)}
-                disabled={savedItem.done == true && true}
-              >
-                <EditIcon />
-              </Button>
-              <Button
-                onClick={() => handleDone(index)}
-                disabled={savedItem.done}
-              >
-                <DoneIcon sx={{ color: savedItem.done ? "" : "green" }} />
-              </Button>
-              <Button onClick={() => handleDelete(index)}>
-                <DeleteIcon color="error" />
-              </Button>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+      )}
+      <List
+        edit={edit}
+        setEdit={setEdit}
+        setTodoValue={setTodoValue}
+        setobj={setobj}
+      />
+    </>
   );
 }
